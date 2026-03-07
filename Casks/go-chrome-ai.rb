271529e83e076768ff69d1af23c4026fdd5a1908
itamaker <itamaker@gmail.com>
2026-03-07 16:27:44 +0800

    *release v1.0.1

--- Casks/go-chrome-ai.rb
+++ Casks/go-chrome-ai.rb
@@ -1,7 +1,7 @@
 cask "go-chrome-ai" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.0.0"
+  version "1.0.1"
   sha256 arm: "f7b54c875ea0000b54860c67d39ddf74b0a033b3353a16c942274e24d9083445",
          intel: "5d5fe38e8c86fb9640577278dc536b4dfd8b15ebbe1e0fd820675e1e17243e58"
 
