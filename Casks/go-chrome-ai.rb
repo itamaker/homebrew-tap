e0fa10dbba0e8418668e9bb89a7d2e0c48df4706
itamaker <itamaker@gmail.com>
2026-03-11 16:10:25 +0800

    Update go-chrome-ai cask to v1.0.3

--- Casks/go-chrome-ai.rb
+++ Casks/go-chrome-ai.rb
@@ -1,9 +1,9 @@
 cask "go-chrome-ai" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.0.2"
-  sha256 arm: "df5d5ea963ed011d1295f375184ee2d4c6281999178f134b3d38f9b815d370b6",
-         intel: "6e10668a4e9147a75925ddbe6ff679f6c34765841f4c292935b47b9608b2bc1b"
+  version "1.0.3"
+  sha256 arm: "dee45018a65de63b6f2ab2a3eb233dacce2d874ddf676b39334232dcbba18612",
+         intel: "04f84b007c0d48ca0e042070059e6cbcbfb424a83ca52a2457c923237187850c"
 
   url "https://github.com/itamaker/go-chrome-ai/releases/download/v#{version}/go-chrome-ai-darwin-#{arch}.tar.gz"
   name "go-chrome-ai"
